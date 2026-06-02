007f44189dbb034201c1f59e671df3ca53d1132e
om-hub-rgb <omy519430@gmail.com>
2026-06-01 21:20:08 -0700

    Ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,15 +2,31 @@ const express = require('express');
 
 require('dotenv').config();
 
+const path = require('path');
+
+
 const app = express();
+const rootdir = path.resolve();
+
+console.log("ROOT:", rootdir);
+console.log("DIST:", path.join(rootdir, "../Frontend/dist"));
 
 const PORT = process.env.PORT || 3000;
 
 const authRoutes = require("./routes/auth.route");
 const messagesRoutes = require("./routes/message.route");
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
+
+app.use(express.static(path.join(rootdir, "../Frontend/dist")));
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(rootdir, "../Frontend/dist/index.html"));
+});
+
 
 app.listen(PORT, () => {console.log("server running on port:" + PORT);
 
